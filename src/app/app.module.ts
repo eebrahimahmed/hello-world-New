4c71e2d38aa79cbd2adfbf81e484e49a8ab77fd5
Ibrahim <Ibrahim@IBRAHIM-WORK>
2018-02-20 20:39:35 +0300

    resolve reflects
    Co-Authored-By: eebrahimahmed <eebrahimahmed@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,8 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
-<<<<<<< HEAD
-=======
 import { SummeryPipe } from './summery.pipe';
 import { FavoriteComponent } from './favorite/favorite.component';
->>>>>>> 0e5e0da901122cbbfe3518f7e004555e575978a5
 
 
 @NgModule({
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CoursesService } from '../courses.service';
-<<<<<<< HEAD
-=======
 import { Alert, Button } from 'selenium-webdriver';
 import { EmailService } from '../email.service';
->>>>>>> 0e5e0da901122cbbfe3518f7e004555e575978a5
 
 @Component({
   selector: 'app-courses',
@@ -12,51 +9,32 @@ import { EmailService } from '../email.service';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
-<<<<<<< HEAD
 
-    isActive="blue";
-    constructor(service : CoursesService) { 
-    this.courses=service.getCourses();
+  isActive = "blue";
+
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
   }
 
-private _courses : string[];
-public get courses() : string[] {
-  return this._courses;
-}
-public set courses(v : string[]) {
-  this._courses = v;
-}
-
-=======
- 
-  constructor(service:CoursesService) { 
-    
-    this.courses= service.getCourses();
+  private _courses: string[];
+  public get courses(): string[] {
+    return this._courses;
   }
-//#region Events
->>>>>>> 0e5e0da901122cbbfe3518f7e004555e575978a5
+  public set courses(v: string[]) {
+    this._courses = v;
+  }
+
+
+  //#region Events
   ngOnInit() {
   }
-  onSave(btnSave)
-  {
-   // console.log ("Message", btnSave );
+  onSave(btnSave) {
+    // console.log ("Message", btnSave );
   }
-  email="eebrahim.ahmed@gmail.com";
-  onInputKeyUp()
-  {
-    
+  email = "eebrahim.ahmed@gmail.com";
+  onInputKeyUp() {
+
     alert(this.email);
   }
   //#endregion
-
-
-  //#region Proprieties
-private _courses : string[];
-  public get courses() : string[] {
-    return this._courses;
-  }
-  public set courses(v : string[]) {
-    this._courses = v;
-  }
-//  #endregion
 }
